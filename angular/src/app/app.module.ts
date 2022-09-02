fbff923872e2396e895866bf4fc7c20b02c2603d
nobilo <nobilopetar@gmail.com>
2022-09-02 11:10:06 +0200

    fix bal-stage

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BalCoreModule } from '@baloise/design-system-next-components-angular';
-import { AppComponent } from './app.component';
-import { PropertyComponent } from './property/property.component';
-import { EventComponent } from './event/event.component';
-import { MethodComponent } from './method/method.component';
-import { FormsSubModule } from './forms/forms-sub.module';
-import { SharedModule } from './shared/shared.module';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BalCoreModule, BalTextModule} from '@baloise/design-system-next-components-angular';
+import {AppComponent} from './app.component';
+import {PropertyComponent} from './property/property.component';
+import {EventComponent} from './event/event.component';
+import {MethodComponent} from './method/method.component';
+import {FormsSubModule} from './forms/forms-sub.module';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -26,9 +26,11 @@ import { SharedModule } from './shared/shared.module';
     }),
     SharedModule,
     FormsSubModule,
+    BalTextModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule {}
+export class AppModule {
+}

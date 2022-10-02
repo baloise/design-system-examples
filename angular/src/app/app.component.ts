6890262b677ebd880123ba12b4b22f038370f297
nobilo <nobilopetar@gmail.com>
2022-10-02 22:52:04 +0200

    add checkbox

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -15,7 +15,8 @@ export class AppComponent {
     name: new FormControl('Example name'),
     radioButton: new FormControl('yes'),
     email: new FormControl(null, [BalValidators.isRequired(), BalValidators.isMinLength(4), BalValidators.isEmail()]),
-    status: new FormControl('Hans Muster')
+    status: new FormControl('Hans Muster'),
+    correct: new FormControl(true)
   });
 
   value = 'input template form'
--- angular/src/app/forms/forms-sub.module.ts
+++ angular/src/app/forms/forms-sub.module.ts
@@ -1,16 +1,17 @@
 import {NgModule} from '@angular/core';
 import {FormComponent} from "./forms-component/form-component.component";
 import {SharedModule} from "../shared/shared.module";
-import {BalRadioModule} from "@baloise/design-system-next-components-angular";
+import {BalCheckboxModule, BalRadioModule} from "@baloise/design-system-next-components-angular";
 
 
 @NgModule({
   declarations: [FormComponent],
   exports: [FormComponent],
-    imports: [
-        SharedModule,
-        BalRadioModule
-    ]
+  imports: [
+    SharedModule,
+    BalRadioModule,
+    BalCheckboxModule
+  ]
 })
 export class FormsSubModule {
 }

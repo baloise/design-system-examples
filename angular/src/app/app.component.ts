ea276064c71a5984997c75437f62b8deed6f1bbc
Timo Klüber <timo.klueber@baloise.ch>
2022-10-24 12:35:09 +0200

    initial app for test initial value for bal-select; https://github.com/baloise-incubator/design-system/issues/603

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { BalConfigState, BalSwissLanguage, onBalConfigChange } from '@baloise/design-system-components'
+import { delay } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+
+export interface SelectionOption<T> {
+  value: T;
+  label: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -10,12 +18,30 @@ import { BalConfigState, BalSwissLanguage, onBalConfigChange } from '@baloise/de
 export class AppComponent {
   title = 'Welcome';
   language: BalSwissLanguage = 'en';
-  form = new FormGroup({});
 
-  constructor() {
+  formGroup: FormGroup = this.fb.group({
+    street: 'basler strasse',
+  });
+
+  options: Observable<SelectionOption<string>[]> = of([
+    {label: 'Basler Strasse', value: 'basler strasse'},
+    {label: 'Züricher Strasse', value: 'züricher strasse'},
+  ]).pipe(
+    delay(2000)
+  )
+
+  constructor(private fb: FormBuilder) {
     onBalConfigChange((config: BalConfigState) => {
       this.language = config.language as BalSwissLanguage;
     });
-
   }
+
+  onSelectedItem($event: CustomEvent<string | string[] | undefined>) {
+    console.log('onSelect', $event);
+  }
+
+  onInput($event: CustomEvent<string>) {
+    console.log('onInput', $event)
+  }
+
 }
